1be7c54961d61d18e3ce22d4f8352d54cd420442
Duong Phi <Phitdgc190826@fpt.edu.vn>
2022-01-06 20:53:44 +0700

    init project

---
+++ AuThenticationAspDonetCore/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace AuThenticationAspDonetCore.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
