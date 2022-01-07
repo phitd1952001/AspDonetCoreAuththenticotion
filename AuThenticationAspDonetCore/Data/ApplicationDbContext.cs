4c9190088aaef67e237234e53590d43c7a44c4dc
Duong Phi <Phitdgc190826@fpt.edu.vn>
2022-01-07 14:33:10 +0700

    add ApplicationUser

--- AuThenticationAspDonetCore/Data/ApplicationDbContext.cs
+++ AuThenticationAspDonetCore/Data/ApplicationDbContext.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Text;
+using AuThenticationAspDonetCore.Models;
 using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore;
 
@@ -12,5 +13,7 @@ namespace AuThenticationAspDonetCore.Data
             : base(options)
         {
         }
+
+        public DbSet<ApplicationUser> ApplicationUsers { get; set; }
     }
 }
---
+++ AuThenticationAspDonetCore/Models/ApplicationUser.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using Microsoft.AspNetCore.Identity;
+
+namespace AuThenticationAspDonetCore.Models
+{
+    public class ApplicationUser: IdentityUser
+    {
+        public String FullName { get; set; }
+        public DateTime  DateOfBirth { get; set; }
+        public string CredentialId{ get; set; }
+        public string HealthCareId { get; set; }
+    }
+}
